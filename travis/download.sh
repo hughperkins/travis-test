11d29b4337312ceebc80940d12a624217c1724e4
Hugh Perkins <hughperkins@gmail.com>
2016-11-27 12:44:38 +0000

    fix path

--- travis/download.sh
+++ travis/download.sh
@@ -2,7 +2,7 @@
 
 BASEL_CACHE=/private/var/tmp/_bazel_travis
 PATH=~/Library/Python/2.7/bin:$PATH
-S3_CACHE_DIR=s3://hughperkinstravis/cache/travis-test/${TRAVIS_BRANCH}/bazel.tar.bz2
+S3_CACHE_DIR=s3://hughperkinstravis/cache/travis-test/${TRAVIS_BRANCH}
 
 aws s3 ls s3://hughperkinstravis
 CACHE_COUNT=$(aws s3 ls ${S3_CACHE_DIR}/bazel.tar.bz2 | wc -l)
--- travis/upload.sh
+++ travis/upload.sh
@@ -2,7 +2,7 @@
 
 BASEL_CACHE=/private/var/tmp/_bazel_travis
 PATH=~/Library/Python/2.7/bin:$PATH
-S3_CACHE_DIR=s3://hughperkinstravis/cache/travis-test/${TRAVIS_BRANCH}/bazel.tar.bz2
+S3_CACHE_DIR=s3://hughperkinstravis/cache/travis-test/${TRAVIS_BRANCH}
 
 cd ${BASEL_CACHE}
 touch bazel.tar.bz2
