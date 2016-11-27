c24d933f528e6b5451bc4737499459e14aa47c12
Hugh Perkins <hughperkins@gmail.com>
2016-11-27 12:23:27 +0000

    update basel cache dirs

--- travis/download.sh
+++ travis/download.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-~/Library/Python/2.7/bin/aws s3 ls s3://hughperkinstravis
-CACHE_COUNT=$(~/Library/Python/2.7/bin/aws s3 ls s3://hughperkinstravis/cache/travis-test/${TRAVIS_BRANCH}/bazel.tar.bz2 | wc -l)
+BASEL_CACHE=/private/var/tmp/_bazel_travis
+PATH=~/Library/Python/2.7/bin:$PATH
+S3_CACHE_DIR=s3://hughperkinstravis/cache/travis-test/${TRAVIS_BRANCH}/bazel.tar.bz2
+
+aws s3 ls s3://hughperkinstravis
+CACHE_COUNT=$(aws s3 ls ${S3_CACHE_DIR}/bazel.tar.bz2 | wc -l)
 echo CACHE_COUNT ${CACHE_COUNT}
 if [[ ${CACHE_COUNT} != 0 ]]; then {
-    mkdir -p ~/.cache/bazel
-    cd ~/.cache/bazel
-    ~/Library/Python/2.7/bin/aws s3 cp s3://hughperkinstravis/cache/travis-test/${TRAVIS_BRANCH}/bazel.tar.bz2 bazel.tar.bz2
+    mkdir -p ${BASEL_CACHE}
+    cd ${BASEL_CACHE}
+    aws s3 cp ${S3_CACHE_DIR}/bazel.tar.bz2 bazel.tar.bz2
     tar -xf bazel.tar.bz2
     ls
 } fi
--- travis/upload.sh
+++ travis/upload.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-cd ~/.cache/bazel
+BASEL_CACHE=/private/var/tmp/_bazel_travis
+PATH=~/Library/Python/2.7/bin:$PATH
+S3_CACHE_DIR=s3://hughperkinstravis/cache/travis-test/${TRAVIS_BRANCH}/bazel.tar.bz2
+
+cd ${BASEL_CACHE}
 touch bazel.tar.bz2
 rm bazel.tar.bz2
 tar -cjf bazel.tar.bz2 *
 ls -lh
-~/Library/Python/2.7/bin/aws s3 cp bazel.tar.bz2 s3://hughperkinstravis/cache/travis-test/${TRAVIS_BRANCH}/bazel.tar.bz2
+aws s3 cp bazel.tar.bz2 ${S3_CACHE_DIR}/bazel.tar.bz2
