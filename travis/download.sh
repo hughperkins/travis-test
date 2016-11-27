71cef98f11267bf0d92c0a81373ce2b43b866b74
Hugh Perkins <hughperkins@gmail.com>
2016-11-27 13:12:04 +0000

    fix if statement for git clone

--- travis/download.sh
+++ travis/download.sh
@@ -8,12 +8,12 @@ PATH=~/Library/Python/2.7/bin:$PATH
 S3_CACHE_DIR=s3://hughperkinstravis/cache/travis-test/${TRAVIS_BRANCH}
 
 # aws s3 ls s3://hughperkinstravis
-CACHE_COUNT=$(aws s3 ls ${S3_CACHE_DIR}/${CACHE_NAME}.tar.bz2 | wc -l)
-echo CACHE_COUNT ${CACHE_COUNT}
-if [[ ${CACHE_COUNT} != 0 ]]; then {
-    mkdir -p ${CACHED_PATH}
-    cd ${CACHED_PATH}
-    time aws s3 cp ${S3_CACHE_DIR}/${CACHE_NAME}.tar.bz2 ${CACHE_NAME}.tar.bz2
-    time tar -xf ${CACHE_NAME}.tar.bz2
-    ls
-} fi
+# CACHE_COUNT=$(aws s3 ls ${S3_CACHE_DIR}/${CACHE_NAME}.tar.bz2 | wc -l)
+# echo CACHE_COUNT ${CACHE_COUNT}
+# if [[ ${CACHE_COUNT} != 0 ]]; then {
+time aws s3 cp ${S3_CACHE_DIR}/${CACHE_NAME}.tar.bz2 /tmp/${CACHE_NAME}.tar.bz2 || exit 0
+mkdir -p ${CACHED_PATH}
+cd ${CACHED_PATH}
+time tar -xf /tmp/${CACHE_NAME}.tar.bz2
+ls
+# } fi
--- travis/upload.sh
+++ travis/upload.sh
@@ -8,8 +8,8 @@ PATH=~/Library/Python/2.7/bin:$PATH
 S3_CACHE_DIR=s3://hughperkinstravis/cache/travis-test/${TRAVIS_BRANCH}
 
 cd ${CACHED_PATH}
-touch ${CACHE_NAME}.tar.bz2
-rm ${CACHE_NAME}.tar.bz2
-time tar -cjf ${CACHE_NAME}.tar.bz2 *
-ls -lh
-time aws s3 cp ${CACHE_NAME}.tar.bz2 ${S3_CACHE_DIR}/${CACHE_NAME}.tar.bz2
+touch /tmp/${CACHE_NAME}.tar.bz2
+rm /tmp/${CACHE_NAME}.tar.bz2
+time tar -cjf /tmp/${CACHE_NAME}.tar.bz2 *
+ls -lh /tmp/${CACHE_NAME}.tar.bz2
+time aws s3 cp /tmp/${CACHE_NAME}.tar.bz2 ${S3_CACHE_DIR}/${CACHE_NAME}.tar.bz2
